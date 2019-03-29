1ef5e0bd48350fde0d11baf90471aef7577696bf
Diogo Duarte <duartediogo98@gmail.com>
2019-03-29 06:35:46 -0400

    adicione o front end

---
+++ app - web/src/App.js
@@ -0,0 +1,72 @@
+import React, { Component } from 'react';
+import { withStyles, MuiThemeProvider, createMuiTheme } from '@material-ui/core/styles';
+import styleApp from './styleApp.jsx';
+import Fab from '@material-ui/core/Fab';
+import CameraAlt from '@material-ui/icons/CameraAlt';
+import blue from '@material-ui/core/colors/blue';
+import { Player } from 'video-react';
+import "../node_modules/video-react/dist/video-react.css"; 
+
+class App extends Component {
+
+  constructor(props) {
+    super(props);
+    this.state = {
+        image: null
+    };
+}
+
+  getPhoto() {
+        alert("entrou aqui2")
+        fetch('http://172.26.250.145:8000/takephoto').then((response) => {
+          console.log(response);
+          this.setState({image: response});
+          alert("response");
+        }).catch((e) => {
+          alert("deu pau")
+        });
+  }
+
+  render() {
+    const theme = createMuiTheme({
+      palette: {
+        primary: blue,
+      },
+      typography: {
+        useNextVariants: true,
+      },
+      fab: {
+        position: 'absolute',
+        bottom: '15px',
+        right: '15px',
+      },
+    });
+    const { classes } = this.props;
+    return (
+      <div className={classes.App}>
+        {/*<div className={classes.videoContainer}>
+          <Player
+            playsInline
+            className={classes.video}
+            poster="/assets/poster.png"
+            src="http://172.26.250.145:8000/video_feed"
+          />
+    </div>*/}
+        <div className={classes.imageContainer}><img className={classes.image} src="http://172.26.250.145:8000/video_feed"></img></div>
+        <img>
+          
+        </img>
+        <div className={classes.buttonContainer}>
+          <MuiThemeProvider theme={theme}>
+            <Fab color="primary" className={classes.fab} onClick={() => this.getPhoto()}>
+              <CameraAlt />
+            </Fab>
+          </MuiThemeProvider>
+        </div>
+      </div>
+    );
+  }
+}
+
+export default withStyles(styleApp)(App);
+
---
+++ app - web/src/styleApp.jsx
@@ -0,0 +1,24 @@
+const styleApp = {
+    videoContainer:{
+        width: "100% !important",
+        height: "50% !important"
+    },
+    video:{
+        marginBottom:"0 !important",
+        padding:"50vh !important"
+    },
+    buttonContainer:{
+        //paddingTop: "15px",
+        //paddingBottom: "15px",
+        top:"50%",
+        right:"15px",
+        position:"absolute",
+        //backgroundColor:"#383838",
+        //textAlign:"center",
+        //width: "100%"
+    },
+    imageContainer:{
+        textAlign: "center"
+    }
+}
+export default styleApp;
