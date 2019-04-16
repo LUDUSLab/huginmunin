5cb42f9da587ade27c6a31dea303ed1f21c7bbe1
Matheus Palheta <matheuspb95@gmail.com>
2019-04-16 19:35:59 -0400

    File Upload

--- node server/hello.js
+++ node server/hello.js
@@ -1,18 +1,38 @@
 #!/usr/bin/env nodejs
 const express = require('express');
-const app = express();
 const helmet = require('helmet');
+const fileType = require('file-type');
+const multer  = require('multer')
+const fs = require('fs');
+const app = express();
+const accepted_extensions = ['jpg', 'png', 'gif'];
+
 app.use(helmet());
-const fileUpload = require('express-fileupload');
-app.use(fileUpload);
+
+const storage = multer.diskStorage({
+  destination: function (req, file, cb) {
+      // error first callback
+      cb(null, 'uploads/');
+  },
+  filename: function (req, file, cb) {
+      // error first callback
+      cb(null, file.fieldname + '-' + Date.now() + '.png') 
+  }
+});
+var upload = multer({ storage, fileFilter: (req, file, cb) => {
+  // if the file extension is in our accepted list
+  if (accepted_extensions.some(ext => file.originalname.endsWith("." + ext))) return cb(null, true);
+  // otherwise, return error
+  return cb(new Error('Only ' + accepted_extensions.join(", ") + ' files are allowed!'));
+}})
 
 app.get('/', function(req, res){  
-  res.send(req);
+  res.send("HEY");
 });
 
-app.post('/upload', function(req, res) {
-  console.log(req.files.foo); // the uploaded file object
-});
+app.post('/upload', upload.single('media'), function (req, res, next) {
+  res.send("UPLOAD")
+})
 
 app.listen(3000, function () {
   console.log('Example app listening on port 3000!');
